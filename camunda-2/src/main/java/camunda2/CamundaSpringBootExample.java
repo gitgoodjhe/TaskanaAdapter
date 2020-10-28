344a23c927705e12f96e958d6c7970e6edc35c56
Joerg Heffner <56156750+gitgoodjhe@users.noreply.github.com>
2020-10-28 12:14:55 +0100

    added second sample camunda

---
+++ camunda-2/src/main/java/camunda2/CamundaSpringBootExample.java
@@ -0,0 +1,13 @@
+package camunda2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaSpringBootExample {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaSpringBootExample.class, args);
+  }
+
+}
